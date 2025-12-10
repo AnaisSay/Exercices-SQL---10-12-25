24539671fd269bc48fa113fefb29badaefc909dd
Anaïs Saï <52666656+AnaisSay@users.noreply.github.com>
2025-12-10 15:55:37 +0100

    Correction

--- TP02.sql
+++ TP02.sql
@@ -1,45 +1,57 @@
--- phpMyAdmin SQL Dump
--- version 5.2.3
--- https://www.phpmyadmin.net/
---
--- Hôte : db
--- Généré le : mer. 10 déc. 2025 à 13:48
--- Version du serveur : 9.5.0
--- Version de PHP : 8.3.28
+-- Création de la base de données si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS COMPTA2;
+USE COMPTA2;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Table des fournisseurs
+CREATE TABLE IF NOT EXISTS FOURNISSEUR (
+    ID INT NOT NULL PRIMARY KEY, 
+    NOM VARCHAR(25) NOT NULL
+);
+
+-- Table des articles avec la clé étrangère ID_FOU qui référence le fournisseur
+CREATE TABLE IF NOT EXISTS ARTICLE (
+    ID INT NOT NULL PRIMARY KEY, 
+    REF VARCHAR(13) NOT NULL, 
+    DESIGNATION VARCHAR(255) NOT NULL, 
+    PRIX DECIMAL(7,2) NOT NULL, 
+    ID_FOU INT NOT NULL,
+    CONSTRAINT FK_ARTICLE_FOU FOREIGN KEY (ID_FOU) REFERENCES FOURNISSEUR(ID)
+);
+
+-- Table des bons de commande avec la clé étrangère ID_FOU qui référence le fournisseur
+CREATE TABLE IF NOT EXISTS BON (
+    ID INT NOT NULL PRIMARY KEY, 
+    NUMERO INT, 
+    DATE_CMDE DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    DELAI INT, 
+    ID_FOU INT NOT NULL,
+    CONSTRAINT FK_BON_FOU FOREIGN KEY (ID_FOU) REFERENCES FOURNISSEUR(ID)
+);
+
+-- Table de composition des bons avec les clés étrangères ID_ART et ID_BON
+CREATE TABLE IF NOT EXISTS COMPO (
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    ID_ART INT NOT NULL, 
+    ID_BON INT NOT NULL, 
+    QTE INT NOT NULL,
+    CONSTRAINT FK_COMPO_ART FOREIGN KEY (ID_ART) REFERENCES ARTICLE(ID),
+    CONSTRAINT FK_COMPO_BON FOREIGN KEY (ID_BON) REFERENCES BON(ID)
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Sélection de la base de données
+USE COMPTA2;
 
---
--- Base de données : `compta`
---
+-- Insertion des fournisseurs
+INSERT INTO FOURNISSEUR (ID, NOM) 
+VALUES 
+(1, 'Française d''Imports'),
+(2, 'FDM SA'),
+(3, 'Dubois & Fils');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `ARTICLE`
---
-
-CREATE TABLE `ARTICLE` (
-  `ID` int NOT NULL,
-  `REF` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `DESIGNATION` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `PRIX` decimal(7,2) NOT NULL,
-  `ID_FOU` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `ARTICLE`
---
-
-INSERT INTO `ARTICLE` (`ID`, `REF`, `DESIGNATION`, `PRIX`, `ID_FOU`) VALUES
+-- Insertion des articles
+INSERT INTO ARTICLE (ID, REF, DESIGNATION, PRIX, ID_FOU) 
+VALUES 
 (1, 'A01', 'Perceuse P1', 74.99, 1),
 (2, 'F01', 'Boulon laiton 4 x 40 mm (sachet de 10)', 2.25, 2),
 (3, 'F02', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, 2),
@@ -49,155 +61,13 @@ INSERT INTO `ARTICLE` (`ID`, `REF`, `DESIGNATION`, `PRIX`, `ID_FOU`) VALUES
 (7, 'A03', 'Perceuse à colonne', 185.25, 1),
 (8, 'D04', 'Coffret mêches à bois', 12.25, 3),
 (9, 'F03', 'Coffret mêches plates', 6.25, 2),
-(10, 'F04', 'Fraises d\'encastrement', 8.14, 2);
+(10, 'F04', 'Fraises d''encastrement', 8.14, 2);
 
--- --------------------------------------------------------
+-- Insertion du bon de commande auprès du fournisseur "Française d'Imports"
+INSERT INTO BON (ID, NUMERO, DELAI, ID_FOU) VALUES (1, 1, 3, 1);
 
---
--- Structure de la table `BON`
---
-
-CREATE TABLE `BON` (
-  `ID` int NOT NULL,
-  `NUMERO` int NOT NULL,
-  `DATE_CMDE` datetime NOT NULL,
-  `DELAI` int NOT NULL,
-  `ID_FOU` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `BON`
---
-
-INSERT INTO `BON` (`ID`, `NUMERO`, `DATE_CMDE`, `DELAI`, `ID_FOU`) VALUES
-(1, 1, '2025-12-10 13:12:11', 3, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `COMPO`
---
-
-CREATE TABLE `COMPO` (
-  `ID` int NOT NULL,
-  `QTE` int NOT NULL,
-  `ID_ART` int NOT NULL,
-  `ID_BON` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `COMPO`
---
-
-INSERT INTO `COMPO` (`ID`, `QTE`, `ID_ART`, `ID_BON`) VALUES
-(1, 3, 1, 1),
-(2, 4, 5, 1),
-(3, 1, 7, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `FOURNISSEUR`
---
-
-CREATE TABLE `FOURNISSEUR` (
-  `ID` int NOT NULL,
-  `NOM` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `FOURNISSEUR`
---
-
-INSERT INTO `FOURNISSEUR` (`ID`, `NOM`) VALUES
-(1, 'Française d\'imports'),
-(2, 'FDM SA'),
-(3, 'Dubois & Fils');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `ARTICLE`
---
-ALTER TABLE `ARTICLE`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_article_fou` (`ID_FOU`);
-
---
--- Index pour la table `BON`
---
-ALTER TABLE `BON`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_bon_fou` (`ID_FOU`);
-
---
--- Index pour la table `COMPO`
---
-ALTER TABLE `COMPO`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_compo_art` (`ID_ART`),
-  ADD KEY `fk_compo_bon` (`ID_BON`);
-
---
--- Index pour la table `FOURNISSEUR`
---
-ALTER TABLE `FOURNISSEUR`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `ARTICLE`
---
-ALTER TABLE `ARTICLE`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT pour la table `BON`
---
-ALTER TABLE `BON`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT pour la table `COMPO`
---
-ALTER TABLE `COMPO`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `FOURNISSEUR`
---
-ALTER TABLE `FOURNISSEUR`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `ARTICLE`
---
-ALTER TABLE `ARTICLE`
-  ADD CONSTRAINT `ARTICLE_ibfk_1` FOREIGN KEY (`ID_FOU`) REFERENCES `FOURNISSEUR` (`ID`);
-
---
--- Contraintes pour la table `BON`
---
-ALTER TABLE `BON`
-  ADD CONSTRAINT `BON_ibfk_1` FOREIGN KEY (`ID_FOU`) REFERENCES `FOURNISSEUR` (`ID`);
-
---
--- Contraintes pour la table `COMPO`
---
-ALTER TABLE `COMPO`
-  ADD CONSTRAINT `COMPO_ibfk_1` FOREIGN KEY (`ID_ART`) REFERENCES `ARTICLE` (`ID`),
-  ADD CONSTRAINT `fk_compo_bon` FOREIGN KEY (`ID_BON`) REFERENCES `BON` (`ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Insertion de la composition du bon de commande n°001
+INSERT INTO COMPO (ID_ART, ID_BON, QTE) VALUES
+(1, 1, 3),  -- 3 Perceuses P1
+(5, 1, 4),  -- 4 Meuleuses 125mm
+(7, 1, 1);  -- 1 Perceuse à colonne
