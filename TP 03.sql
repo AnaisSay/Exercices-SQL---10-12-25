e9b975516b03ba72394d71b1f1d9010721ab6fe9
Anaïs Saï <52666656+AnaisSay@users.noreply.github.com>
2025-12-10 15:58:40 +0100

    plus simple

--- TP 03.sql
+++ TP 03.sql
@@ -9,7 +9,7 @@ SELECT * FROM `ARTICLE` WHERE PRIX BETWEEN 2 AND 6.25;
 -- e. Listez tous les articles, dans l'ordre des prix descendants, et dont le prix n'est pas compris entre 2 et 6.25 euros et dont le fournisseur est Française d'Imports.
 SELECT * FROM ARTICLE
 WHERE PRIX NOT BETWEEN 2 AND 6.25
-AND ID_FOU = (SELECT ID FROM FOURNISSEUR WHERE NOM = 'Française d''Imports')
+AND ID_FOU = 1
 ORDER BY PRIX DESC;
 -- f. En utilisant un opérateur logique, listez tous les articles dont les fournisseurs sont la Française d'imports ou Dubois et Fils
 SELECT * FROM `ARTICLE` WHERE ID_FOU = 1 || ID_FOU = 3; 
@@ -20,3 +20,4 @@ SELECT * FROM `ARTICLE` WHERE ID_FOU NOT IN (1,3);
 -- i. Listez tous les bons de commande dont la date de commande est entre le 01/02/2019 et le 30/04/2019.
 
 SELECT * FROM BON WHERE DATE_CMDE BETWEEN '2019-02-01' AND '2019-04-30'; 
+
